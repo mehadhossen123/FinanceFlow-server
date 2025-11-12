30cd97659a8295a64ba86def735f7ee8db5c0d2c
Mehad Hossen <mehad004938@gmail.com>
2025-11-12 11:35:23 +0600

    remove connect .client

--- index.js
+++ index.js
@@ -61,7 +61,7 @@ const client = new MongoClient(uri, {
 
 async function run() {
   try {
-    await client.connect();
+    // await client.connect();
 
     const database = client.db("Assignment10DB");
 
@@ -165,10 +165,10 @@ async function run() {
       }
     });
 
-    await client.db("admin").command({ ping: 1 });
-    console.log(
-      "Pinged your deployment. You successfully connected to MongoDB!"
-    );
+    // // await client.db("admin").command({ ping: 1 });
+    // console.log(
+    //   "Pinged your deployment. You successfully connected to MongoDB!"
+    // );
   } finally {
   }
 }
